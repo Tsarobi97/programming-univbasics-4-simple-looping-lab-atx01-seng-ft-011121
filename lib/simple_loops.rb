0915572aaa46ca357fa78248180f457a711b5f72
Robert Obi <robobi97@googlemail.com>
2020-12-28 17:49:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,10 +1,9 @@
 # Write your methods here
-
+#array = Hello World
 def loop_message_five_times(array)
-  count = 0 
-  while count < 5 do
+  count = 0
+  While count < 5 do
     puts array
-    count += 1 
-  end 
-end 
-  
+    count += 1
+  end
+end
